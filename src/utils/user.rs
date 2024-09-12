7b66c8e4b44053c9c7baa8808f5c41ac0a1f68f8
371tti <109227273+371tti@users.noreply.github.com>
2024-09-13 01:27:53 +0900

    とりあえず動くように修正　リポジトリのチェック

--- src/utils/user.rs
+++ src/utils/user.rs
@@ -1,26 +1,26 @@
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
-use crate::sys::init::AppConfig;
+// use crate::sys::init::AppConfig;
 
 
-pub struct UserData {
-    user_name: String,
-    user_id: String,
-    account_level: i32,
-    password: String,
+// pub struct UserData {
+//     user_name: String,
+//     user_id: String,
+//     account_level: i32,
+//     password: String,
 
-}
+// }
 
-pub struct User {
-    users: HashMap<u128, Option<UserData>>,
-    id_to_ruid: HashMap<String, u128>
-}
+// pub struct User {
+//     users: HashMap<u128, Option<UserData>>,
+//     id_to_ruid: HashMap<String, u128>
+// }
 
-impl User {
-    pub fn new(app_config: AppConfig, mongodb: ) -> Self {
+// impl User {
+//     pub fn new(app_config: AppConfig, mongodb: ) -> Self {
 
-    }
+//     }
 
-    fn 
-}
+//     fn 
+// }

a0ddee56e2c8ae549095882fe8fdc60ac5dfbd69
371tti <109227273+371tti@users.noreply.github.com>
2024-10-21 23:50:06 +0900

    app_setをお掃除

--- src/sys/app_set.rs
+++ src/sys/app_set.rs
@@ -3,8 +3,6 @@ use crate::{build_handlers::{file_api::FileApi, json_api::JsonApi}, db_handlers:
 use super::init::AppConfig;
 
 pub struct AppSet {
-    pub file_api: FileApi,
-    pub json_api: JsonApi,
     pub session: Session,
     pub ruid: RuidGenerator,
     pub db: MongoClient,
@@ -15,18 +13,13 @@ pub struct AppSet {
 
 impl AppSet {
     pub async fn new(app_config: AppConfig) -> Self{
-        let json_api = JsonApi::new(&app_config);
         let ruid = RuidGenerator::new(&app_config);
         let session = Session::new(&app_config);
         let db = MongoClient::new(&app_config).await.expect("dbへの接続失敗　パニックなう"); // あとでエラーハンドリングする
         let ws = WsConnectionSet::new(&app_config);
-        
-        let file_api = FileApi::new(&app_config, &json_api);
         let user = User::new(&app_config, &db);
 
         let app_mod = AppSet {
-            file_api: file_api,
-            json_api: json_api,
             session: session,
             ruid: ruid,
             db: db,
